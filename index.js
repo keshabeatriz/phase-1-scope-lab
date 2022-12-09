6e944b0b1595e71203310a26f13fccb2e55dd639
Kesha <kesha.marges@gmail.com>
2022-12-09 15:49:16 -0500

    commit tree f0b12d9e4fc3ee643419fec10ffece12ab6eba75

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'aina';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'wesley';
+
+    return leastFavoriteCustomer;
+}
+
+changeLeastFavoriteCustomer();
